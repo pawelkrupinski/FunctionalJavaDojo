904406cf3e37f91842cb2c2f64ae9156ac237e65
Pawel Krupinski <pawel.krupinski@youdevise.com>
2012-05-31 16:51:19 +0100

    DRY solution

--- src/main/java/org/timgroup/dojo/FootballMunging.java
+++ src/main/java/org/timgroup/dojo/FootballMunging.java
@@ -2,45 +2,22 @@ package org.timgroup.dojo;
 
 import fj.data.Stream;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import static java.lang.Integer.valueOf;
 
 public class FootballMunging {
 
-    private final Stream<String> lines;
+    private GenericMunging<String> munging;
 
     public FootballMunging(Stream<String> lines) {
-        this.lines = lines;
+        this.munging = new GenericMunging<String>(lines);
     }
 
     public String teamWithSmallestDifferenceBetweenForAndAgainst() {
-        final List<FootballData> data = new ArrayList<FootballData>();
-        for (String line : lines) {
-            final String cleanLine = line.trim().replaceAll("\\*", "");
-            if (cleanLine.matches("^\\d.*")) {
-                final String[] fields = cleanLine.split("\\s+");
-                data.add(new FootballData(fields[1], valueOf(fields[6]), valueOf(fields[8])));
+        return munging.munge(new Converter<String>() {
+            @Override
+            public GenericMunging.MungingData<String> convert(String[] fields) {
+                return new GenericMunging.MungingData<String>(fields[1], Math.abs(valueOf(fields[6]) - valueOf(fields[8])));
             }
-        }
-        Collections.sort(data);
-        return data.get(0).team;
-    }
-
-    private class FootballData implements Comparable<FootballData> {
-        private final String team;
-        private int spread;
-
-        public FootballData(String team, int forPoints, int againstPoints) {
-            this.team = team;
-            this.spread = Math.abs(forPoints - againstPoints);
-        }
-
-        @Override
-        public int compareTo(FootballData footballData) {
-            return spread - footballData.spread;
-        }
+        });
     }
 }
--- src/main/java/org/timgroup/dojo/WeatherMunging.java
+++ src/main/java/org/timgroup/dojo/WeatherMunging.java
@@ -2,44 +2,20 @@ package org.timgroup.dojo;
 
 import fj.data.Stream;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import static java.lang.Integer.valueOf;
 
 public class WeatherMunging {
-    private final Stream<String> lines;
+    private GenericMunging<Integer> munging;
 
     public WeatherMunging(Stream<String> lines) {
-        this.lines = lines;
+        this.munging = new GenericMunging<Integer>(lines);
     }
 
     public int dayWithTheSmallestTemperatureSpread() {
-        final List<WeatherData> data = new ArrayList<WeatherData>();
-        for (String line : lines) {
-            final String cleanLine = line.trim().replaceAll("\\*", "");
-            if (cleanLine.matches("^\\d.*")) {
-                final String[] fields = cleanLine.split("\\s+");
-                data.add(new WeatherData(valueOf(fields[0]), valueOf(fields[1]), valueOf(fields[2])));
+        return munging.munge(new Converter<Integer>() {
+            @Override public GenericMunging.MungingData<Integer> convert(String[] fields) {
+                return new GenericMunging.MungingData<Integer>(valueOf(fields[0]), valueOf(fields[1]) - valueOf(fields[2]));
             }
-        }
-        Collections.sort(data);
-        return data.get(0).day;
-    }
-
-    public static class WeatherData implements Comparable<WeatherData> {
-        private final int day;
-        private final int spread;
-
-        public WeatherData(int day, int max, int min) {
-            this.day = day;
-            this.spread = max - min;
-        }
-
-        @Override
-        public int compareTo(WeatherData weatherData) {
-            return spread - weatherData.spread;
-        }
+        });
     }
 }
